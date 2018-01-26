335a6200fd42efdd57a3520a9b2b007ebbf3bd34
Antony Laget <antony.laget@icloud.com>
2018-01-26 16:41:18 +0100

    C refactoring

---
+++ src/level-1/dcc-c/cmake-build-debug/CMakeFiles/dcc_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dcc_c.dir/main.c.o"
+  "dcc_c.pdb"
+  "dcc_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dcc_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
