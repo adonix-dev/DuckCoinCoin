384041c36edd4e2c940f872c84104f259abd6364
Antony Laget <antony.laget@icloud.com>
2018-01-26 16:21:47 +0100

    Refactoring
    :100:

--- src/level-1/dcc-j/src/dcc_gui/dcc_gui.java
+++ src/level-1/dcc-j/src/dcc_gui/dcc_gui.java
@@ -1,29 +1,30 @@
-package src.ddc_gui;
+package dcc_gui;
 
 import javax.swing.*;
-//import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ddc_gui {
+public class dcc_gui {
+
     private JButton button;
     private JPanel panelMain;
 
-    public ddc_gui() {
+    public dcc_gui() {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null, "Hello World");
             }
         });
-   }
+    }
 
     public static void main(String[] args){
-        JFrame frame = new JFrame("ddc_gui");
-        frame.setContentPane(new ddc_gui().panelMain);
+
+        JFrame frame = new JFrame("dcc_gui");
+        frame.setContentPane(new dcc_gui().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        System.out.println("hello world");
     }
+
 }
